0dad3b5ae458e49b5f2f2fdd5fddb04280e31bf1
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2023-08-11 12:24:52 -0300

    new(master): add header

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
+
+import ShoppingCart from './src/pages/ShoppingCart';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Shopping Cart</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <StatusBar />
+      <ShoppingCart />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
