4d144c88601248d02f9e5d64c9522d1876f15d45
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2023-08-16 15:40:31 -0300

    new(master): product screen

--- src/pages/ShoppingCart/index.tsx
+++ src/pages/ShoppingCart/index.tsx
@@ -1,8 +1,41 @@
-import React from 'react'
-import { Image } from 'react-native'
+import React from "react";
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
 
-import topo from '../../../assets/topo.png'
+import topo from "../../../assets/topo.png";
+
+const screenWidth = Dimensions.get("screen").width;
+
+const imageHeight = 578;
+const imageWidth = 768;
 
 export default function ShoppingCart() {
-  return <Image source={topo} />
+  return (
+    <>
+      <Image source={topo} style={style.topo} />
+      <Text style={style.titulo}>Detalhes do Carrinho</Text>
+      <View>
+        <Text>Carrinho de Compras</Text>
+        <Text>Fazenda ETEC Cidade do Livro</Text>
+        <Text>Uma cesta com produtos selecionados cuidadosamente</Text>
+        <Text>R$40,00</Text>
+      </View>
+    </>
+  );
 }
+
+const style = StyleSheet.create({
+  topo: {
+    width: "100%",
+    height: (imageHeight / imageWidth) * screenWidth,
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: "bold",
+    lineHeight: 26,
+    width: "100%",
+    textAlign: "center",
+    color: "white",
+    position: "absolute",
+    padding: 16,
+  },
+});
