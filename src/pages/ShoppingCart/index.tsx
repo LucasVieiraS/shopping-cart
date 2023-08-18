8224faf69e9740126c2b260e99a4584dd6c63bda
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2023-08-18 11:51:13 -0300

    refactor(master): style & translation
    - styled elements
    - translated from PT-BR to EN-US

--- src/pages/ShoppingCart/index.tsx
+++ src/pages/ShoppingCart/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
 
-import topo from "../../../assets/topo.png";
+import top from "../../../assets/top.png";
+import logo from "../../../assets/logo.png";
 
 const screenWidth = Dimensions.get("screen").width;
 
@@ -11,12 +12,15 @@ const imageWidth = 768;
 export default function ShoppingCart() {
   return (
     <>
-      <Image source={topo} style={style.topo} />
-      <Text style={style.titulo}>Detalhes do Carrinho</Text>
-      <View>
-        <Text>Carrinho de Compras</Text>
-        <Text>Fazenda ETEC Cidade do Livro</Text>
-        <Text>Uma cesta com produtos selecionados cuidadosamente</Text>
+      <Image source={top} style={style.top} />
+      <Text style={style.title}>Shopping Cart Details</Text>
+      <View style={style.cartView}>
+        <Text style={style.cartTitle}>Shopping Cart</Text>
+        <View style={style.farmView}>
+          <Image source={logo} style={style.farmImage} />
+          <Text style={style.farmName}>ETEC Cidade do Livro's Farm</Text>
+        </View>
+        <Text style={style.description}>A basket filled with sorted products</Text>
         <Text>R$40,00</Text>
       </View>
     </>
@@ -24,11 +28,11 @@ export default function ShoppingCart() {
 }
 
 const style = StyleSheet.create({
-  topo: {
+  top: {
     width: "100%",
     height: (imageHeight / imageWidth) * screenWidth,
   },
-  titulo: {
+  title: {
     fontSize: 24,
     fontWeight: "bold",
     lineHeight: 26,
@@ -37,5 +41,32 @@ const style = StyleSheet.create({
     color: "white",
     position: "absolute",
     padding: 16,
+  },
+  description: {
+    color: '#A3A3A3',
+    fontStyle: 'italic',
+  },
+  cartTitle: {
+    fontSize: 26,
+    lineHeight: 42,
+    color: "#464646",
+  },
+  cartView: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  farmName: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 12,
+    fontWeight: 'bold',
+  },
+  farmImage: {
+    width: 32,
+    height: 32,
+  },
+  farmView: {
+    flexDirection: 'row',
+    paddingVertical: 12,
   },
 });
